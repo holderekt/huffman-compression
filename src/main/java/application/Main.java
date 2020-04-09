5f9babd4e0ecb384543c7dac783c1c510e1e0b9c
Ivan Diliso <navispeshd@gmail.com>
2020-04-09 12:01:36 +0200

    Initial Commmit

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,11 @@
+package application;
+import main.java.bitstream.*;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        BitOutputStream mario = new BitOutputStream("mario.txt");
+
+    }
+}
